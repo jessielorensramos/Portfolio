d7a2090d9408811d2a73ade3e38ce2bbac47a833
Jessie Ramos <jessielorensramos@gmail.com>
2025-04-09 17:17:43 +0800

    Making animation for text

---
+++ web.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multiple-text",{
+    strings: ["UI/UX Designer", "Web Developer", "Freelancer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 100,
+    loop: true,
+})
